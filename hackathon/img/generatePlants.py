fcbed2b13d57d6ba16a999b79933b5e25a19758d
14tp00 <57301686+14tp00@users.noreply.github.com>
2021-10-03 15:24:04 +0200

    mapa
    generacja tablicy

---
+++ hackathon/img/generatePlants.py
@@ -0,0 +1,11 @@
+# import numpy as np
+# import random
+#
+# def generatePlant(n, map):
+#     w = len(map[0])
+#     h = len(map)
+#
+#     x = random.randrange(0,w,1)
+#     y = random.randrange(0,h,1)
+#
+#     if map[x][y] ==
---
+++ hackathon/main.py
@@ -0,0 +1,52 @@
+from kivy.app import App
+from kivy.graphics import Rectangle
+from kivy.graphics import Color
+from kivy.uix.widget import Widget
+from kivy.config import Config
+
+import generateMatrix as gM
+
+# set width and height of app window
+width = 800
+height = 800
+Config.set('graphics', 'width', width)
+Config.set('graphics', 'height', height)
+
+
+class Touch(Widget):
+    def __init__(self, **kwargs):
+        super(Touch, self).__init__(**kwargs)
+
+        # get all location arrays
+        array_water = gM.getArray('./img/map_water.png', 1)
+        array_food = gM.getArray('./img/map_food.png', 2)
+        array_obstacle = gM.getArray('./img/map_obstacle.png', 3)
+
+        array = gM.MergeMaps(array_food, array_water, array_obstacle)
+
+        w = len(array[0])
+        h = len(array)
+
+        with self.canvas:
+            for i in range(0,h):
+                for j in range(0, w):
+                    #for one point in every array
+                    if array[i,j] == 1: #draw rect if pt is water
+                        Color(0, 0, 1, 0.5, mode="rgba")
+                        self.rect = Rectangle(pos=((height/h)*j, abs((width/w)*i-(width-width/w))), size=(height/h,width/w))
+
+                    if array[i,j] == 2: #draw rect if pt is f00d
+                        Color(0, 1, 0, 0.5, mode="rgba")
+                        self.rect = Rectangle(pos=((height/h)*j, abs((width/w)*i-(width-width/w))), size=(height/h,width/w))
+
+                    if array[i,j] == 3: #draw rect if pt is obstacle
+                        Color(1, 0, 0, 0.5, mode="rgba")
+                        self.rect = Rectangle(pos=((height/h)*j, abs((width/w)*i-(width-width/w))), size=(height/h,width/w))
+
+class MainApp(App):
+    def build(self):
+        return Touch()
+
+if __name__ == '__main__':
+    app = MainApp()
+    app.run()
